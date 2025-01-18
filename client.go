3cc7dc03b95064da4f4a3d777e68dea0f88a1a74
PiccoloMondoC <119958916+PiccoloMondoC@users.noreply.github.com>
2025-01-17 21:34:15 -0600

    Add files via upload

--- client.go
+++ client.go
@@ -2,7 +2,12 @@
 package audittraillib
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/google/uuid"
@@ -50,4 +55,128 @@ func NewClient(baseURL string, token string, apiKey string, httpClient ...*http.
 		Token:      token,
 		ApiKey:     apiKey,
 	}
+}
+
+func (c *Client) LogAction(auditTrail AuditTrail) (bool, error) {
+	apiUrl := fmt.Sprintf("%s/log-action", c.BaseURL)
+
+	jsonData, err := json.Marshal(auditTrail)
+	if err != nil {
+		return false, err
+	}
+
+	req, err := http.NewRequest(http.MethodPost, apiUrl, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return false, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+c.Token)
+	req.Header.Set("X-API-Key", c.ApiKey)
+
+	resp, err := c.HttpClient.Do(req)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+
+	return resp.StatusCode == http.StatusCreated, nil
+}
+
+// Helper function for GET requests
+func (c *Client) getAuditTrail(apiUrl string) ([]AuditTrail, error) {
+	var auditTrails []AuditTrail
+
+	req, err := http.NewRequest(http.MethodGet, apiUrl, nil)
+	if err != nil {
+		return auditTrails, err
+	}
+
+	req.Header.Set("Authorization", "Bearer "+c.Token)
+	req.Header.Set("X-API-Key", c.ApiKey)
+
+	resp, err := c.HttpClient.Do(req)
+	if err != nil {
+		return auditTrails, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return auditTrails, err
+	}
+
+	err = json.Unmarshal(body, &auditTrails)
+	if err != nil {
+		return auditTrails, err
+	}
+
+	return auditTrails, nil
+}
+
+func (c *Client) GetAuditTrailByEntityID(entityID uuid.UUID) ([]AuditTrail, error) {
+	apiUrl := fmt.Sprintf("%s/audit-trail/entity/%s", c.BaseURL, entityID.String())
+	return c.getAuditTrail(apiUrl)
+}
+
+func (c *Client) GetAuditTrailByUserID(userID uuid.UUID) ([]AuditTrail, error) {
+	apiUrl := fmt.Sprintf("%s/audit-trail/user/%s", c.BaseURL, userID.String())
+	return c.getAuditTrail(apiUrl)
+}
+
+func (c *Client) GetAuditTrailByDateRange(startDate, endDate time.Time) ([]AuditTrail, error) {
+	apiUrl := fmt.Sprintf("%s/audit-trail/date-range/%s/%s", c.BaseURL, startDate.Format(time.RFC3339), endDate.Format(time.RFC3339))
+	return c.getAuditTrail(apiUrl)
+}
+
+func (c *Client) GetAuditTrailByActionType(actionType string) ([]AuditTrail, error) {
+	apiUrl := fmt.Sprintf("%s/audit-trail/action-type/%s", c.BaseURL, strings.TrimSpace(actionType))
+	return c.getAuditTrail(apiUrl)
+}
+
+func (c *Client) GetAuditTrailByEntityName(entityName string) ([]AuditTrail, error) {
+	apiUrl := fmt.Sprintf("%s/audit-trail/entity-name/%s", c.BaseURL, strings.TrimSpace(entityName))
+	return c.getAuditTrail(apiUrl)
+}
+
+func (c *Client) postAuditTrail(apiUrl string, body io.Reader) ([]AuditTrail, error) {
+	var auditTrails []AuditTrail
+
+	req, err := http.NewRequest(http.MethodPost, apiUrl, body)
+	if err != nil {
+		return auditTrails, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+c.Token)
+	req.Header.Set("X-API-Key", c.ApiKey)
+
+	resp, err := c.HttpClient.Do(req)
+	if err != nil {
+		return auditTrails, err
+	}
+	defer resp.Body.Close()
+
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return auditTrails, err
+	}
+
+	err = json.Unmarshal(responseBody, &auditTrails)
+	if err != nil {
+		return auditTrails, err
+	}
+
+	return auditTrails, nil
+}
+
+func (c *Client) GetFilteredAuditTrail(filter *AuditTrailFilter) ([]AuditTrail, error) {
+	apiUrl := fmt.Sprintf("%s/getFilteredAuditTrail", c.BaseURL)
+
+	filterJson, err := json.Marshal(filter)
+	if err != nil {
+		return nil, err
+	}
+
+	return c.postAuditTrail(apiUrl, bytes.NewBuffer(filterJson))
 }
